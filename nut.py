6c0b19e02f1c75ac30375402a173c8c7a59ef8b0
nutizxcx <nutzxcx@hotmail.com>
2020-03-05 21:10:47 +0700

    update broker

---
+++ nut.py
@@ -0,0 +1,23 @@
+topicMsg = {'ip1':[], 'ip2':[], 'ip3':[], 'ip4':[]}
+topicList = {'topic1': ['ip1','ip2','ip3','ip4']}
+
+def send_message(client_socket, address):
+  while True:
+    data = topicMsg[address(0)].pop(0)
+    client_socket.send(data.encode('utf-8'))
+  client_socket.close()
+
+
+# def publisher_service(topic, message):
+#   try:
+#     sub_ip = topicList[topic]
+#   except KeyError:
+#       print("Topic does not exist")
+#   else:
+#      for sb in sub_ip:
+#       topicMsg[sb].append(message)
+#       print(topicMsg)
+  
+# publisher_service('topic1', 'this is message')
+# publisher_service('topic2', 'this is message')
+# publisher_service('topic3', 'this is message')
