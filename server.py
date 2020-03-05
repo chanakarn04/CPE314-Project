4c06998522eb278e271700ce3be6f0f3df81fb5e
nutizxcx <nutzxcx@hotmail.com>
2020-03-05 22:12:10 +0700

    edit broker

--- server.py
+++ server.py
@@ -32,19 +32,19 @@ def send_message(client_socket, address):
     client_socket.send(data.encode('utf-8'))
   client_socket.close()
 
-# def publisher_service(topic, message):
-#   try:
-#     sub_ip = topicList[topic]
-#   except KeyError:
-#       print("Topic does not exist")
-#   else:
-#      for sb in sub_ip:
-#       topicMsg[sb].append(message)
-#       print(topicMsg)
+def publisher_service(topic, message):
+  try:
+    sub_ip = topicList[topic]
+  except KeyError:
+      print("Topic does not exist")
+  else:
+     for sb in sub_ip:
+      topicMsg[sb].append([topic, message])
+      print(topicMsg)
   
 # publisher_service('topic1', 'this is message')
 # publisher_service('topic2', 'this is message')
 # publisher_service('topic3', 'this is message')
 
-if __name__ == '__main__':
-    server()
+# if __name__ == '__main__':
+#     server()
