d5135459eedda7bd7adf0af74b31fbd7b26fc502
Pisut-1037 <pisut.peter@mail.kmutt.ac.th>
2020-03-07 09:15:40 +0700

    change exit method

--- publisher.py
+++ publisher.py
@@ -1,5 +1,5 @@
 import socket
-import sys
+import sys, os
 
 def publish():
     SERV_PORT = 50000
@@ -10,11 +10,22 @@ def publish():
     s = socket.socket()
     s.connect(addr)
     s.send(data.encode('utf-8'))
-    while True:
-        txtout = input("Publisher>> ")
-        s.send(txtout.encode('utf-8'))
-        if txtout == 'quit':
-            break
+    try:
+        while True:
+            txtout = input("Publisher>> ")
+            s.send(txtout.encode('utf-8'))
+            if txtout == 'q':
+                break
+    except KeyboardInterrupt:
+        print ('Interrupted ..')
+        try:
+            message = 'q'
+            s.send(message.encode('utf-8'))
+            s.close()
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
+    s.close()
 
-
-publish()
+if __name__ == "__main__":
+    publish()
